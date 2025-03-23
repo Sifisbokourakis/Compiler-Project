ec851048779dfaa86b84d291fda5d44cef295f3b
Sifis Bokourakis <sifisbokourakis@gmail.com>
2025-03-23 14:00:07 +0200

    The second phase of the intermediate code is working and shows the expected results

--- greek++_4439_4883.py
+++ greek++_4439_4883.py
@@ -10,8 +10,8 @@ alphabito =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
 
 noumera =['0','1','2','3','4','5','6','7','8','9']
 
-#file = open(sys.argv[1],'r',encoding='utf-8')
-file = open('check2.gre', 'r', encoding='utf-8')
+file = open(sys.argv[1],'r',encoding='utf-8')
+#file = open('check2.gre', 'r', encoding='utf-8')
 
 space = 0
 letters = 1
@@ -1013,7 +1013,7 @@ def syntax():
             else:
                 print("Error: There is no '[' at the start. ", line)
                 exit(-1)
-
+            
         elif(res[0] == left_bracket_tk):
             res = lex()
             line = res[2]
@@ -1266,9 +1266,9 @@ def backPatch(list,z):
 
     for i in range(len(list)):
          for j in range(len(quadLst)):
-             if(list[1] == quadLst[j][0] and quadLst[j][4] == '_'):
-                 quadLst[j][4] = z
-                 break
+                if(list[i] == quadLst[j][0] and quadLst[j][4] == '_'):
+                    quadLst[j][4] = z
+                    break
     return
 '''
      for num in list:
